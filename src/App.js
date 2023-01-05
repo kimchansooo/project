b4b430b36392bc0cc781d2c765eb2afb3af4e986
kimchansooo <kcs50604914@gmail.com>
2023-01-05 09:37:57 +0900

    최종

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import Detail from './components/Detail';
 import data from './data';
 import Detail2 from './components/Detail2';
 import Tool from './components/Tool';
+import Main from './components/Main';
 
 export let StockContext = React.createContext();
 
 function App() {
   let [shirts, setShirts] = useState(data);
   let [stock] = useState([7, 13, 20]);
+  let [flag, setFlag] = useState(true);
 
 
   return (
@@ -48,13 +50,15 @@ function App() {
           <Nav className="ms-auto">반갑습니다. 5조입니다.</Nav>
         </Container>
       </Navbar>
-      {/* <div className='col-md-8 col-sm-12'>
+       {/* <div className='col-md-2'>
         <div className='row'>
-      <img src='https://w.namu.la/s/939277e2b6d14bc7ecaf94d189458e1b68b7f538dd1c258acf04290b84db93875e6d4101b5cc21a0b7dcc45513758c18503e58fc6f53277c6d0898cef44c3e75defbde69d95c1691ea3c6c18542ec5badcc270d138b873f1bf36fb952d40398e414f3e545410281a0280aac2cbfba11a'/>
-      <img src='https://e7.pngegg.com/pngimages/560/216/png-clipart-book-graphics-illustration-book-book-reading.png'/>
+     
       </div>
-      </div> */}
+      </div>  */}
+
+
       <Routes>
+        <Route path='/' element={<Main></Main>}/>
         <Route path='/page' element={<Page/>}/>
         <Route
           path='/detail2/:id'
---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap";
+
+function Main(){
+    return(
+    <>
+    <Container>
+        <div className="col-md-12 col-sm-12">
+    <div className="row">
+    <div className="col-md-6">
+        <img src='https://w.namu.la/s/939277e2b6d14bc7ecaf94d189458e1b68b7f538dd1c258acf04290b84db93875e6d4101b5cc21a0b7dcc45513758c18503e58fc6f53277c6d0898cef44c3e75defbde69d95c1691ea3c6c18542ec5badcc270d138b873f1bf36fb952d40398e414f3e545410281a0280aac2cbfba11a'/>
+     </div>
+      <img src='https://en.pimg.jp/017/062/101/1/17062101.jpg'/>
+
+    </div>
+    </div>
+    </Container>
+
+    </>)
+}
+export default Main;
